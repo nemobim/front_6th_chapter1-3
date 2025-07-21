9d18b4736f7dce29597f15bbcf90afc2e9c9622f
nemobim <drakequation@naver.com>
2025-07-22 02:57:10 +0900

    feat: memo, deepMemo 구현

--- packages/lib/src/hocs/memo.ts
+++ packages/lib/src/hocs/memo.ts
@@ -1,6 +1,20 @@
 import { type FunctionComponent } from "react";
 import { shallowEquals } from "../equals";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(Component: FunctionComponent<P>, equals = shallowEquals) {
-  return Component;
+  return function MemoizedComponent(props: P) {
+    //이전 props(현재 props와 비교하기 위해)
+    const prevPropsRef = useRef<P | null>(null);
+    //이전 결과(현재 결과와 비교하기 위해)
+    const prevResultRef = useRef<ReturnType<typeof Component> | null>(null);
+
+    //이전 props와 현재 props가 다르면 새로운 결과를 반환
+    if (!prevPropsRef.current || !equals(prevPropsRef.current, props)) {
+      prevPropsRef.current = props;
+      prevResultRef.current = Component(props);
+    }
+
+    return prevResultRef.current;
+  };
 }
