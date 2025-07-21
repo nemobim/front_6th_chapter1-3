37a6245c3530d0b88b9da81bd7bbc74334e5c4c1
nemobim <drakequation@naver.com>
2025-07-22 00:54:43 +0900

    feat:shallowEquals 함수 구현

--- packages/lib/src/equals/shallowEquals.ts
+++ packages/lib/src/equals/shallowEquals.ts
@@ -1,3 +1,30 @@
 export const shallowEquals = (a: unknown, b: unknown) => {
-  return a === b;
+  // 기본값 === 비교
+  if (a === b) return true;
+
+  // null 체크(null의 타입도 객체이므로 체크)
+  if (a == null || b == null) return false;
+
+  // 타입이 다르면 false
+  if (typeof a !== typeof b) return false;
+
+  // 객체나 배열인 경우
+  if (typeof a === "object") {
+    const aKeys = Object.keys(a);
+    const bKeys = Object.keys(b);
+
+    // 키 개수 다르면 false
+    if (aKeys.length !== bKeys.length) return false;
+
+    // 각 key의 값이 === 인지 확인
+    for (const key of aKeys) {
+      if (!b.hasOwnProperty(key)) return false;
+      if (a[key as keyof typeof a] !== b[key as keyof typeof b]) return false;
+    }
+
+    return true;
+  }
+
+  // 나머지는 === 비교로 처리
+  return false;
 };
