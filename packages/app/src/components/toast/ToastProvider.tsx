52fe1d01ba4371ae731cdd4f26ce387c24a58a29
nemo-nemo-bim <drakequation@naver.com>
2025-07-24 02:21:44 +0900

    fix: Toast Provider 최적화

--- packages/app/src/components/toast/ToastProvider.tsx
+++ packages/app/src/components/toast/ToastProvider.tsx
@@ -2,51 +2,57 @@
 import { createContext, memo, type PropsWithChildren, useContext, useReducer } from "react";
 import { createPortal } from "react-dom";
 import { Toast } from "./Toast";
-import { createActions, initialState, toastReducer, type ToastType } from "./toastReducer";
+import { createActions, initialState, toastReducer, type ToastState, type ToastType } from "./toastReducer";
 import { debounce } from "../../utils";
+import { useAutoCallback } from "@hanghae-plus/lib";
+import { useMemo } from "@hanghae-plus/lib/src/hooks";
 
 type ShowToast = (message: string, type: ToastType) => void;
 type Hide = () => void;
 
-const ToastContext = createContext<{
-  message: string;
-  type: ToastType;
+const ToastStateContext = createContext<ToastState>(initialState);
+const ToastDispatchContext = createContext<{
   show: ShowToast;
   hide: Hide;
 }>({
-  ...initialState,
   show: () => null,
   hide: () => null,
 });
 
 const DEFAULT_DELAY = 3000;
 
-const useToastContext = () => useContext(ToastContext);
+const useToastDispatchContext = () => useContext(ToastDispatchContext);
 export const useToastCommand = () => {
-  const { show, hide } = useToastContext();
+  const { show, hide } = useToastDispatchContext();
   return { show, hide };
 };
+
+const useToastStateContext = () => useContext(ToastStateContext);
 export const useToastState = () => {
-  const { message, type } = useToastContext();
+  const { message, type } = useToastStateContext();
   return { message, type };
 };
 
 export const ToastProvider = memo(({ children }: PropsWithChildren) => {
   const [state, dispatch] = useReducer(toastReducer, initialState);
-  const { show, hide } = createActions(dispatch);
+  const { show, hide } = useMemo(() => createActions(dispatch), [dispatch]);
   const visible = state.message !== "";
 
-  const hideAfter = debounce(hide, DEFAULT_DELAY);
-
-  const showWithHide: ShowToast = (...args) => {
+  const hideAfter = useAutoCallback(() => debounce(hide, DEFAULT_DELAY));
+  const showWithHide: ShowToast = useAutoCallback((...args) => {
     show(...args);
     hideAfter();
-  };
+  });
+
+  const stateValue = useMemo(() => ({ message: state.message, type: state.type }), [state.message, state.type]);
+  const actionValue = useMemo(() => ({ show: showWithHide, hide }), [showWithHide, hide]);
 
   return (
-    <ToastContext value={{ show: showWithHide, hide, ...state }}>
-      {children}
-      {visible && createPortal(<Toast />, document.body)}
-    </ToastContext>
+    <ToastStateContext.Provider value={stateValue}>
+      <ToastDispatchContext.Provider value={actionValue}>
+        {children}
+        {visible && createPortal(<Toast />, document.body)}
+      </ToastDispatchContext.Provider>
+    </ToastStateContext.Provider>
   );
 });
